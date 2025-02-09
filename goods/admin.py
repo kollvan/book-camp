132eb60de613a06850e56df3c61b2235766b4095
Nikem <username@gmail.com>
2025-02-09 11:00:57 +0300

    Create. Model tag

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from goods.models import Category, Tag
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'slug']
+    prepopulated_fields = {'slug':('name', )}
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    fields = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
--- goods/models.py
+++ goods/models.py
@@ -12,3 +12,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='Slug')
+
+    class Meta:
+        db_table = 'tag'
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
